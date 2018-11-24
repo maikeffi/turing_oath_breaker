538e07a1860b84e552739eff5330cd2e56a0d683
root <root@10-118-166-62.ebiz.verizon.com>
2018-11-24 02:38:08 -0500

    adding parallel

---
+++ nltk_download.py
@@ -0,0 +1,3 @@
+import nltk
+nltk.data.path.append("/prod/nltk_data")
+nltk.download('punkt',download_dir='/prod/nltk_data')
--- text_classifier.py
+++ text_classifier.py
@@ -7,21 +7,18 @@ Created on Tue Nov 20 18:15:05 2018
 """
 import re
 import pandas as pd
-import nltk
+import time
 from collections import Counter
-import matplotlib.pyplot as plt
 import numpy as np
 import nltk 
-nltk.data.path.append("/var/lib/docker/nltk_data")
+nltk.data.path.append("/prod/nltk_data/")
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
-import seaborn as sns
 from textblob import TextBlob
 from textblob import Word
 from pywsd.utils import lemmatize_sentence
-
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import LinearSVC
@@ -31,16 +28,17 @@ from sklearn.model_selection import train_test_split ,GridSearchCV
 from sklearn.metrics import classification_report,accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import RandomizedSearchCV
+from sklearn.externals.joblib import Parallel, delayed
 
+
+start_time = time.time()
 stop_words = set(stopwords.words('english'))
 stop_words.add('The')
 stop_words.add('This')
 ps = PorterStemmer()
 lemmatizer = WordNetLemmatizer()
 
-data = pd.read_csv('data/ads_en_us.csv')
-fax = sns.countplot(x="subcatid", data=data)
-plt.show()
+data = pd.read_csv('/prod/starlord/turing_oath_breaker/data/ads_en_us.csv')
 
 def remove_stop_words(sentence):
     word_tokens = word_tokenize(sentence)
@@ -85,14 +83,17 @@ data_27s = data_27.sample(frac=0.5)
 data1 = data[data.subcatid != 27]
 data_s = pd.concat([data1, data_27s], axis = 0 )
 data_bal=data_s.groupby('subcatid').filter(lambda x: len(x) >= 100)
-data_bal['value_processed'] = data_s.value.apply(clean_str)
-data_bal['value_processed_lemm'] = data_bal.value_processed.apply(perform_lemm)
+"""
+#data_bal['value_processed'] = data_s.value.apply(clean_str)
+#data_bal['value_processed_lemm'] = data_bal.value_processed.apply(perform_lemm)
+"""
+data_bal['value_processed'] = Parallel(n_jobs=-1, verbose=10)(delayed(clean_str)(i) for i in data_bal['value'].tolist())
+data_bal['value_processed_lemm'] = Parallel(n_jobs=-1, verbose=10)(delayed(perform_lemm)(i) for i in data_bal['value_processed'].tolist())
+
+print(data_bal.value_processed_lemm.head(20))
 
 
-data_bal.value_processed_lemm.head(20)
-
-
-X = data_bal.value_processed_lemm
+X = data_bal.value_processed
 y = data_bal.subcatid
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state = 5)
 
@@ -135,3 +136,4 @@ gs_clf_rfc.fit(X_train, y_train)
 model = gs_clf_rfc.best_estimator_
 y_pred = model.predict(X_test)
 print('accuracy %s' % accuracy_score(y_pred, y_test))
+print("--- %s seconds --- " %(time.time()-start_time))
